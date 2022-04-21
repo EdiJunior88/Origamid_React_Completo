bd40901e8b37d13d1c09c64a5ff5b19197975a1c
EdiJunior88 <er.moura88@yahoo.com.br>
2022-04-21 20:41:52 -0300

    JSX 2

--- 02_React_Para_Iniciantes/Capitulo_08/meuapp/src/App.js
+++ 02_React_Para_Iniciantes/Capitulo_08/meuapp/src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 /*
 Mostre os dados da aplicação, como aprensetado no vídeo
+
 Não utilize CSS externo, use o style para mudar as cores
+
 Se a situação estiver ativa pinte de verde, inativa vermelho
+
 Se o gasto for maior que 10000 mostre uma mensagem
+
 const luana = {
   cliente: 'Luana',
   idade: 27,
