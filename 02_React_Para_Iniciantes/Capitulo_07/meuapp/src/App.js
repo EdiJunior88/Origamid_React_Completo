e90fb5b44564cbd1657982b0664567c11fdcc007
EdiJunior88 <er.moura88@yahoo.com.br>
2022-04-20 21:13:17 -0300

    JSX 1

---
+++ 02_React_Para_Iniciantes/Capitulo_07/meuapp/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const titulo = <h1>Esse é um título</h1>
+
+const App = () => {
+  const random = Math.random();
+  const ativo = false;
+
+  function mostrarNome(sobrenome) {
+    return "André " + sobrenome;
+  }
+
+  const carro = {
+    marca: "Ford",
+    rodas: 4,
+  }
+
+  const estiloP = {
+    color: "blue",
+    fontSize: "2rem",
+  }
+
+  return (
+    <>
+      {titulo}
+      <p>{true ? "asasas" : "eeeeee"} - {10}
+        {mostrarNome("Rafael")}
+      </p>
+      <p style={estiloP}>{new Date().getFullYear()}</p>
+      <p style={estiloP}>{carro.marca}</p>
+      <p>{carro.rodas}</p>
+      <p className={ativo ? 'ativo' : 'inativo'}>{random * 1000 / 50}</p>
+    </>
+  )
+};
+
+export default App;
