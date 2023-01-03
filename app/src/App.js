e3fff644d0cf651d0839528ed174ec605d504b3c
EdiJunior88 <er.moura88@yahoo.com.br>
2023-01-03 20:31:01 -0300

    0201 Adicionar React 0202 React Básico 0203 React e JavaScript 0204 Webpack e Babel 0205 npx create-react-app 0206 Ambiente Curso 0207 JSX 1 0207 JSX 2 0208 JSX Arrays 1 0208 JSX Arrays 2

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,47 @@
 import React from "react";
 
-const App = () => {
-  const livros = [
-    { nome: "A Game of Thrones", ano: 1996 },
-    { nome: "A Clash of Kings", ano: 1998 },
-    { nome: "A Storm of Swords", ano: 2000 },
-  ];
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: "Smartphone",
+    preco: "R$ 2000",
+    cores: ["#29d8d5", "#252a34", "#fc3766"],
+  },
+  {
+    id: 2,
+    nome: "Notebook",
+    preco: "R$ 3000",
+    cores: ["#ffd045", "#d4394b", "#f37c59"],
+  },
+  {
+    id: 3,
+    nome: "Tablet",
+    preco: "R$ 1500",
+    cores: ["#365069", "#47c1c8", "#f95786"],
+  },
+];
 
+const App = () => {
   return (
-    <ul>
-      {livros
-        .filter(
-          ({ ano }) =>
-            (ano) >=
-              1998
-        )
-        .map(({ nome, ano }) => (
-          <li key={nome}>
-            {nome}, {ano}
-          </li>
+    <section>
+      {produtos
+        .filter((produto) => Number(produto.preco.replace("R$ ", "")) > 1500)
+        .map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+            <p>Preço: {produto.preco}</p>
+            <ul>
+              {produto.cores.map((cor) => (
+                <li key={cor} style={{ backgroundColor: cor, color: "white" }}>
+                  {cor}
+                </li>
+              ))}
+            </ul>
+          </div>
         ))}
-    </ul>
+    </section>
   );
 };
 
