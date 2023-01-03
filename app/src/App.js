3ea46fca7a965c616ee2d1a2778a9dbe7ba2849b
EdiJunior88 <er.moura88@yahoo.com.br>
2023-01-03 09:11:19 -0300

    0201 Adicionar React 0202 React Básico 0203 React e JavaScript 0204 Webpack e Babel 0205 npx create-react-app 0206 Ambiente Curso 0207 JSX 1

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,38 @@
 import React from "react";
 
+const titulo = <h1>Esse é um título</h1>;
+
 const App = () => {
-  return <div>Meu App</div>;
+  const random = Math.random();
+  const ativo = true;
+
+  function mostrarNome(sobrenome) {
+    return "André " + sobrenome;
+  }
+
+  const carro = {
+    marca: "Ford",
+    rodas: 4,
+  };
+
+  const estiloP = {
+    color: "blue",
+    fontSize: "2rem",
+  };
+
+  return (
+    <>
+      {titulo}
+      <p>
+        {true ? "tetete" : "aaaaaa"} - {10}
+        {mostrarNome("Rafael")}
+      </p>
+      <p style={estiloP}>{new Date().getFullYear()}</p>
+      <p style={estiloP}>{carro.marca}</p>
+      <p>{carro.rodas}</p>
+      <p className={ativo ? "ativo" : "inativo"}>{(random * 1000) / 50}</p>
+    </>
+  );
 };
 
 export default App;
