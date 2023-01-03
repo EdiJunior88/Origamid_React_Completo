c99edf724f6676bec551f64562150dc8438ed2dc
EdiJunior88 <er.moura88@yahoo.com.br>
2023-01-03 20:40:52 -0300

    0201 Adicionar React 0202 React Básico 0203 React e JavaScript 0204 Webpack e Babel 0205 npx create-react-app 0206 Ambiente Curso 0207 JSX 1 0207 JSX 2 0208 JSX Arrays 1 0208 JSX Arrays 2 0209 Eventos

--- app/src/App.js
+++ app/src/App.js
@@ -1,47 +1,18 @@
 import React from "react";
 
-// Organize os produtos como mostrado no vídeo
-// Mostre apenas produtos que forem mais caros que R$ 1500
-const produtos = [
-  {
-    id: 1,
-    nome: "Smartphone",
-    preco: "R$ 2000",
-    cores: ["#29d8d5", "#252a34", "#fc3766"],
-  },
-  {
-    id: 2,
-    nome: "Notebook",
-    preco: "R$ 3000",
-    cores: ["#ffd045", "#d4394b", "#f37c59"],
-  },
-  {
-    id: 3,
-    nome: "Tablet",
-    preco: "R$ 1500",
-    cores: ["#365069", "#47c1c8", "#f95786"],
-  },
-];
-
 const App = () => {
+  function handleScroll(event) {
+    console.log(event);
+  }
+
+  window.addEventListener("scroll", handleScroll);
+
   return (
-    <section>
-      {produtos
-        .filter((produto) => Number(produto.preco.replace("R$ ", "")) > 1500)
-        .map((produto) => (
-          <div key={produto.id}>
-            <h1>{produto.nome}</h1>
-            <p>Preço: {produto.preco}</p>
-            <ul>
-              {produto.cores.map((cor) => (
-                <li key={cor} style={{ backgroundColor: cor, color: "white" }}>
-                  {cor}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-    </section>
+    <div style={{ height: "200vh" }}>
+      <button onClick={(event) => alert(event.target.innerText)}>
+        Compre Violão
+      </button>
+    </div>
   );
 };
 
