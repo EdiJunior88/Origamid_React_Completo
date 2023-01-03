f26e050c1e5816f4c36c6acdd10fa6318eb60090
EdiJunior88 <er.moura88@yahoo.com.br>
2023-01-02 22:04:35 -0300

    0201 Adicionar React 0202 React Básico 0203 React e JavaScript 0204 Webpack e Babel 0205 npx create-react-app

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,9 @@
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
