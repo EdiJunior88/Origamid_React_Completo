0536db9e6d1fc059898c5c4f553c59b2e1138bd0
EdiJunior88 <er.moura88@yahoo.com.br>
2023-01-03 08:41:06 -0300

    0201 Adicionar React 0202 React Básico 0203 React e JavaScript 0204 Webpack e Babel 0205 npx create-react-app 0206 Ambiente Curso

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div>Meu App</div>;
+};
 
-export default App
+export default App;
