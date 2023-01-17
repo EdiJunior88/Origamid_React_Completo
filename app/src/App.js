996e81d269c64c6409196155069c78e3b1b0aa6e
EdiJunior88 <er.moura88@yahoo.com.br>
2023-01-16 22:32:04 -0300

    0301 useState 1 0301 useState 2 0301 useState 3

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React from 'react'
+import Produto from './Produto';
 
 const App = () => {
-  const [contar, setContar] = React.useState(1);
-  const [itens, setItens] = React.useState(["Item 1"]);
+  const [dados, setDados] = React.useState(null);
+  const [carregando, setCarregando] = React.useState(null);
 
-  function handleClick() {
-    setContar(contar + 1);
-    setItens([...itens, "Item " + (contar + 1)]);
+  async function handleClick(event) {
+    setCarregando(true);
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+    );
+    const json = await response.json();
+    setDados(json);
+    setCarregando(false);
   }
 
   return (
-    <div>
-      {itens.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
-      <button onClick={handleClick}>{contar}</button>
-    </div>
+    <>
+      <button onClick={handleClick}>smartphone</button>
+      <button onClick={handleClick}>tablet</button>
+      <button onClick={handleClick}>notebook</button>
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <Produto dados={dados} />}
+    </>
   );
-};
+}
 
-export default App;
+export default App
