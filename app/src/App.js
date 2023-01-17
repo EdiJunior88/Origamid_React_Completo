424fc57afd87ac2c022de7fdd7987d04a9f22c64
EdiJunior88 <er.moura88@yahoo.com.br>
2023-01-16 21:23:01 -0300

    0301 useState 1

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,16 @@
 import React from "react";
-import Header from "./Header";
-import Home from "./Home";
-import Produtos from "./Produtos";
+import ButtonModal from "./ButtonModal";
+import Modal from "./Modal";
 
 const App = () => {
-  const { pathname } = window.location;
-
-  let Component;
-  if (pathname === "/produtos") {
-    Component = Produtos;
-  } else {
-    Component = Home;
-  }
+  const [modal, setModal] = React.useState(false);
 
   return (
-    <section>
-      <Header />
-      <Component />
-    </section>
+    <div>
+      <div>{modal ? "Modal Aberto" : "Modal Fechado"}</div>
+      <Modal modal={modal} setModal={setModal}/>
+      <ButtonModal setModal={setModal} />
+    </div>
   );
 };
 
