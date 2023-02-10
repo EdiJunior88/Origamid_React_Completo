2fcc7180e42b2b080a8c5042a00f266318436bb9
EdiJunior88 <er.moura88@yahoo.com.br>
2023-02-09 21:06:44 -0300

    0303 useRef

--- app/src/App.js
+++ app/src/App.js
@@ -1,37 +1,26 @@
-// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
-// Defina o produto clicado como uma preferência do usuário no localStorage
-// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
-
-import React from 'react';
-import Produto from './Produto';
+import React, { useRef, useState } from "react";
 
 const App = () => {
-  const [produto, setProduto] = React.useState(null);
+  const [carrinho, setCarrinho] = useState(0);
+  const [notificacao, setNotificacao] = useState(null);
+  const timeoutRef = useRef();
 
-  React.useEffect(() => {
-    const produtoLocal = window.localStorage.getItem('produto');
-    if (produtoLocal !== 'null') setProduto(produtoLocal);
-  }, []);
+  function handleClick() {
+    setCarrinho(carrinho + 1);
+    setNotificacao("Item adicionado ao carrinho");
 
-  React.useEffect(() => {
-    if (produto !== null) window.localStorage.setItem('produto', produto);
-  }, [produto]);
-
-  function handleClick({ target }) {
-    setProduto(target.innerText);
+    clearTimeout(timeoutRef.current);
+    
+    timeoutRef.current = setTimeout(() => {
+      setNotificacao(null);
+    }, 2000);
+    console.log(timeoutRef.current);
   }
 
   return (
     <div>
-      <h1>Preferência: {produto}</h1>
-      <button style={{ marginRight: '1rem' }} onClick={handleClick}>
-        notebook
-      </button>
-      <button onClick={handleClick}>smartphone</button>
-      <Produto produto={produto} />
+      <p>{notificacao}</p>
+      <button onClick={handleClick}>Adicionar Carrinho {carrinho}</button>
     </div>
   );
 };
