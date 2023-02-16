299923a9aab871a2dd053cf96ada7997c0b49b52
EdiJunior88 <er.moura88@yahoo.com.br>
2023-02-16 13:29:49 -0300

    0401 Input 2

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,48 @@
-import React, { useState } from "react";
+// Faça um fetch (POST) para a API abaixo
+// Para a criação ser aceita é necessário enviar dodos de:
+// nome, email, senha, cep, rua, numero, bairro, cidade e estado
+
+// Essa é a função utilizado para realizar o POST
+/* fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  // form é o objeto com os dados do formulário
+  body: JSON.stringify(form),
+}); */
+
+// Mostre uma mensagem na tela, caso a resposta da API seja positiva
+
+
+import React from 'react';
 
 const App = () => {
-  // const [nome, setNome] = useState("");
-  // const [email, setEmail] = useState("");
-
-  const [form, setForm] = useState({
-    nome: "",
-    email: "",
+  const [form, setForm] = React.useState({
+    nome: '',
+    email: '',
+    senha: '',
+    cep: '',
+    rua: '',
+    numero: '',
+    bairro: '',
+    cidade: '',
+    estado: '',
   });
+
+  const [response, setResponse] = React.useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(event);
+    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    }).then((response) => {
+      setResponse(response);
+    });
   }
 
   function handleChange({ target }) {
@@ -21,27 +52,56 @@ const App = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor='nome'>Nome</label>
+      <label htmlFor="nome">Nome</label>
+      <input type="text" id="nome" value={form.nome} onChange={handleChange} />
+      <label htmlFor="email">Email</label>
       <input
-        id='nome'
-        type='text'
-        name='nome'
-        value={form.nome}
-        onChange={handleChange}
-      />
-      {form.nome}
-
-      <label htmlFor='email'>Email</label>
-      <input
-        id='email'
-        type='email'
-        name='email'
+        type="email"
+        id="email"
         value={form.email}
         onChange={handleChange}
       />
-      {form.email}
-      
+      <label htmlFor="senha">Senha</label>
+      <input
+        type="password"
+        id="senha"
+        value={form.senha}
+        onChange={handleChange}
+      />
+      <label htmlFor="cep">Cep</label>
+      <input type="text" id="cep" value={form.cep} onChange={handleChange} />
+      <label htmlFor="senha">Rua</label>
+      <input type="text" id="rua" value={form.rua} onChange={handleChange} />
+      <label htmlFor="numero">Número</label>
+      <input
+        type="text"
+        id="numero"
+        value={form.numero}
+        onChange={handleChange}
+      />
+      <label htmlFor="bairro">Bairro</label>
+      <input
+        type="text"
+        id="bairro"
+        value={form.bairro}
+        onChange={handleChange}
+      />
+      <label htmlFor="cidade">Cidade</label>
+      <input
+        type="text"
+        id="cidade"
+        value={form.cidade}
+        onChange={handleChange}
+      />
+      <label htmlFor="estado">Estado</label>
+      <input
+        type="text"
+        id="estado"
+        value={form.estado}
+        onChange={handleChange}
+      />
       <button>Enviar</button>
+      {response && response.ok && <p>Usuário Criado</p>}
     </form>
   );
 };
