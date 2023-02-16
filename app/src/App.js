3a353dae82d39654ef9b5305138a30644391305e
EdiJunior88 <er.moura88@yahoo.com.br>
2023-02-16 17:01:45 -0300

    0404 Radio

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,58 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [select, setSelect] = useState("");
+  const [produto, setProduto] = useState("");
+  const [cor, setCor] = useState("");
+
+  function handleChange({ target }) {
+    setProduto(target.value);
+  }
 
   return (
     <form>
-      <select
-        value={select}
-        id='produtos'
-        onChange={({ target }) => setSelect(target.value)}>
-        <option disabled value=''>
-          Selecione
-        </option>
-        <option value='notebook'>Notebook</option>
-        <option value='smartphone'>Smartphone</option>
-        <option value='tablet'>Tablet</option>
-      </select>
-      {select}
+      <h2>Cor</h2>
+      <label>
+        <input
+          type='radio'
+          onChange={({ target }) => setCor(target.value)}
+          checked={cor === "azul"}
+          value='azul'
+        />
+        Azul
+      </label>
+
+      <label>
+        <input
+          type='radio'
+          onChange={({ target }) => setCor(target.value)}
+          checked={cor === "vermelho"}
+          value='vermelho'
+        />
+        Vermelho
+      </label>
+
+      <h2>Produtos</h2>
+      <label>
+        <input
+          type='radio'
+          name='produto'
+          onChange={handleChange}
+          checked={produto === "smartphone"}
+          value='smartphone'
+        />
+        Smartphone
+      </label>
+
+      <label>
+        <input
+          type='radio'
+          name='produto'
+          onChange={handleChange}
+          checked={produto === "notebook"}
+          value='notebook'
+        />
+        Notebook
+      </label>
     </form>
   );
 };
