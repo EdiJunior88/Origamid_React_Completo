1e837625afec2ee653ccf7f24e8f2b259d286e52
EdiJunior88 <er.moura88@yahoo.com.br>
2023-02-16 16:41:00 -0300

    0402 Textarea

--- app/src/App.js
+++ app/src/App.js
@@ -1,107 +1,16 @@
-// Faça um fetch (POST) para a API abaixo
-// Para a criação ser aceita é necessário enviar dodos de:
-// nome, email, senha, cep, rua, numero, bairro, cidade e estado
-
-// Essa é a função utilizado para realizar o POST
-/* fetch('https://ranekapi.origamid.dev/json/api/usuario', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  // form é o objeto com os dados do formulário
-  body: JSON.stringify(form),
-}); */
-
-// Mostre uma mensagem na tela, caso a resposta da API seja positiva
-
-
-import React from 'react';
+import React, { useState } from "react";
 
 const App = () => {
-  const [form, setForm] = React.useState({
-    nome: '',
-    email: '',
-    senha: '',
-    cep: '',
-    rua: '',
-    numero: '',
-    bairro: '',
-    cidade: '',
-    estado: '',
-  });
-
-  const [response, setResponse] = React.useState(null);
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(form),
-    }).then((response) => {
-      setResponse(response);
-    });
-  }
-
-  function handleChange({ target }) {
-    const { id, value } = target;
-    setForm({ ...form, [id]: value });
-  }
+  const [textarea, setTextarea] = useState("");
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="nome">Nome</label>
-      <input type="text" id="nome" value={form.nome} onChange={handleChange} />
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        id="email"
-        value={form.email}
-        onChange={handleChange}
+    <form action=''>
+      <textarea
+        value={textarea}
+        onChange={({ target }) => setTextarea(target.value)}
+        rows="5"
       />
-      <label htmlFor="senha">Senha</label>
-      <input
-        type="password"
-        id="senha"
-        value={form.senha}
-        onChange={handleChange}
-      />
-      <label htmlFor="cep">Cep</label>
-      <input type="text" id="cep" value={form.cep} onChange={handleChange} />
-      <label htmlFor="senha">Rua</label>
-      <input type="text" id="rua" value={form.rua} onChange={handleChange} />
-      <label htmlFor="numero">Número</label>
-      <input
-        type="text"
-        id="numero"
-        value={form.numero}
-        onChange={handleChange}
-      />
-      <label htmlFor="bairro">Bairro</label>
-      <input
-        type="text"
-        id="bairro"
-        value={form.bairro}
-        onChange={handleChange}
-      />
-      <label htmlFor="cidade">Cidade</label>
-      <input
-        type="text"
-        id="cidade"
-        value={form.cidade}
-        onChange={handleChange}
-      />
-      <label htmlFor="estado">Estado</label>
-      <input
-        type="text"
-        id="estado"
-        value={form.estado}
-        onChange={handleChange}
-      />
-      <button>Enviar</button>
-      {response && response.ok && <p>Usuário Criado</p>}
+      {textarea}
     </form>
   );
 };
