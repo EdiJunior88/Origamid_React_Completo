36c90709c3a30734d3dfa9048a9cba6cb9716ca6
EdiJunior88 <er.moura88@yahoo.com.br>
2023-02-21 10:27:22 -0300

    0407 Componentes Select

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import Input from "./Form/Input";
+import Select from "./Form/Select";
 
 const App = () => {
-  const [nome, setNome] = useState("")
-  const [email, setEmail] = useState("")
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [produto, setProduto] = useState("");
 
   return (
     <form>
-      <Input id="nome" label="Nome"  value={nome} setValue={setEmail} required />
-      <Input id="email" label="Email" value={email} setValue={setNome} />
+      <Select
+        options={["Smartfone", "Tablet"]}
+        value={produto}
+        setValue={setProduto}
+      />
+      <Input id='nome' label='Nome' value={nome} setValue={setEmail} required />
+      <Input id='email' label='Email' value={email} setValue={setNome} />
       <button>Enviar</button>
     </form>
   );
