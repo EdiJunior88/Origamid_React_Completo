585b9669559828a9c6973b528fb3b4e304c17fcb
EdiJunior88 <er.moura88@yahoo.com.br>
2023-02-21 09:58:42 -0300

    0405 Checkbox 2

--- app/src/App.js
+++ app/src/App.js
@@ -1,37 +1,47 @@
-import React, { useState } from "react";
+// Otimize o código do slide anterior
+// Utilizando a array abaixo para mostrar
+// cada checkbox na tela.
+
+// const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
+
+import React from "react";
+
+const coresArray = ["azul", "roxo", "laranja", "verde", "vermelho", "cinza"];
 
 const App = () => {
-  const [cores, setCores] = useState(["azul"]);
+  const [cores, setCores] = React.useState([]);
 
   function handleChange({ target }) {
-    if (target.checked) {
+    const { checked, value } = target;
+    if (checked) {
       setCores([...cores, target.value]);
     } else {
-      setCores(cores.filter((cor) => cor !== target.value));
+      setCores(cores.filter((cor) => cor !== value));
     }
+  }
+
+  function handleChecked(cor) {
+    return cores.includes(cor);
   }
 
   return (
     <form>
-      <label>
-        <input
-          type='checkbox'
-          value='azul'
-          checked={cores.includes("azul")}
-          onChange={handleChange}
-        />
-        Azul
-      </label>
-
-      <label>
-        <input
-          type='checkbox'
-          value='vermelho'
-          checked={cores.includes("vermelho")}
-          onChange={handleChange}
-        />
-        Vermelho
-      </label>
+      {coresArray.map((cor) => (
+        <label key={cor} style={{ textTransform: "capitalize" }}>
+          <input
+            type='checkbox'
+            value={cor}
+            checked={handleChecked(cor)}
+            onChange={handleChange}
+          />
+          {cor}
+        </label>
+      ))}
+      <ul>
+        {cores.map((cor) => (
+          <li key={cor}>{cor}</li>
+        ))}
+      </ul>
     </form>
   );
 };
