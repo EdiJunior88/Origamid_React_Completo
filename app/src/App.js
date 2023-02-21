d8758005aeea9d8294769948b876d53f373f1393
EdiJunior88 <er.moura88@yahoo.com.br>
2023-02-21 10:35:45 -0300

    0408 Componentes Radio

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import Input from "./Form/Input";
+import Radio from "./Form/Radio";
 import Select from "./Form/Select";
 
 const App = () => {
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [produto, setProduto] = useState("");
+  const [cor, setCor] = useState("Vermelho");
+  const [frutas, setFrutas] = useState("Vermelho");
 
   return (
     <form>
+      <h2>Cores</h2>
+      <Radio options={["Azul", "Vermelho"]} value={cor} setValue={setCor} />
+
+      <h2>Frutas</h2>
+      <Radio
+        options={["Limão", "Laranja", "Uva"]}
+        value={frutas}
+        setValue={setFrutas}
+      />
+
       <Select
         options={["Smartfone", "Tablet"]}
         value={produto}
