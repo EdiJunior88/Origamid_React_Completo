218aa4c86a528b061ae2ad972c4d061fa1822c66
EdiJunior88 <er.moura88@yahoo.com.br>
2023-02-21 20:27:21 -0300

    0412 Desafio Formulários

--- app/src/App.js
+++ app/src/App.js
@@ -1,35 +1,87 @@
-import React from "react";
-import Input from "./Form/Input";
-import useForm from "./Hooks/useForm";
+import React from 'react';
+import Radio from './Form/Radio';
+
+const perguntas = [
+  {
+    pergunta: 'Qual método é utilizado para criar componentes?',
+    options: [
+      'React.makeComponent()',
+      'React.createComponent()',
+      'React.createElement()',
+    ],
+    resposta: 'React.createElement()',
+    id: 'p1',
+  },
+  {
+    pergunta: 'Como importamos um componente externo?',
+    options: [
+      'import Component from "./Component"',
+      'require("./Component")',
+      'import "./Component"',
+    ],
+    resposta: 'import Component from "./Component"',
+    id: 'p2',
+  },
+  {
+    pergunta: 'Qual hook não é nativo?',
+    options: ['useEffect()', 'useFetch()', 'useCallback()'],
+    resposta: 'useFetch()',
+    id: 'p3',
+  },
+  {
+    pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
+    options: ['set', 'get', 'use'],
+    resposta: 'use',
+    id: 'p4',
+  },
+];
 
 const App = () => {
-  const cep = useForm("cep");
-  const email = useForm("email");
-  const nome = useForm();
-  const sobrenome = useForm(false);
+  const [respostas, setRespostas] = React.useState({
+    p1: '',
+    p2: '',
+    p3: '',
+    p4: '',
+  });
+  const [slide, setSlide] = React.useState(0);
+  const [resultado, setResultado] = React.useState(null);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (cep.validate() && email.validate() && nome.validate()) {
-      console.log("Enviar");
+  function handleChange({ target }) {
+    setRespostas({ ...respostas, [target.id]: target.value });
+  }
+
+  function resultadoFinal() {
+    const corretas = perguntas.filter(
+      ({ id, resposta }) => respostas[id] === resposta,
+    );
+    setResultado(`Você acertou: ${corretas.length} de ${perguntas.length}`);
+  }
+
+  function handleClick() {
+    if (slide < perguntas.length - 1) {
+      setSlide(slide + 1);
     } else {
-      console.log("Não enviar");
+      setSlide(slide + 1);
+      resultadoFinal();
     }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Input label='Nome' id='nome' type='text' {...nome} />
-      <Input label='Sobrenome' id='sobrenome' type='text' {...sobrenome} />
-      <Input
-        label='CEP'
-        id='cep'
-        type='text'
-        placeholder='00000-000'
-        {...cep}
-      />
-      <Input label='Email' id='email' type='email' {...email} />
-      <button>Enviar</button>
+    <form onSubmit={(event) => event.preventDefault()}>
+      {perguntas.map((pergunta, index) => (
+        <Radio
+          active={slide === index}
+          key={pergunta.id}
+          value={respostas[pergunta.id]}
+          onChange={handleChange}
+          {...pergunta}
+        />
+      ))}
+      {resultado ? (
+        <p>{resultado}</p>
+      ) : (
+        <button onClick={handleClick}>Próxima</button>
+      )}
     </form>
   );
 };
--- app/src/Form/Radio.js
+++ app/src/Form/Radio.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React from 'react';
 
-const Radio = ({ options, value, setValue, ...props }) => {
+const Radio = ({ pergunta, options, onChange, value, id, active }) => {
+  if (active === false) return null;
   return (
-    <>
+    <fieldset
+      style={{
+        padding: '2rem',
+        marginBottom: '1rem',
+        border: '2px solid #eee',
+      }}
+    >
+      <legend style={{ fontWeight: 'bold' }}>{pergunta}</legend>
       {options.map((option) => (
-        <label key={option}>
+        <label
+          key={option}
+          style={{ marginBottom: '1rem', fontFamily: 'monospace' }}
+        >
           <input
-            type='radio'
-            value={option}
+            type="radio"
+            id={id}
             checked={value === option}
-            onChange={({ target }) => setValue(target.value)}
-            {...props}
+            value={option}
+            onChange={onChange}
           />
           {option}
         </label>
       ))}
-    </>
+    </fieldset>
   );
 };
 
