bc8b937d484982e2128e7b5a550d30733d9b6e66
EdiJunior88 <er.moura88@yahoo.com.br>
2023-02-22 20:29:14 -0300

    0505 Animações 1

--- app/src/App.js
+++ app/src/App.js
@@ -1,29 +1,15 @@
-import React from "react";
-import Card from "react-bootstrap/Card";
+import React, { useState } from "react";
+import Produto from "./Produto";
+import "./App.css"
 
 const App = () => {
-  return (
-    //Bootstrap padrão
-    // <div className='card bg-dark text-white m-5' style={{ maxWidth: "18rem" }}>
-    //   <div className='card-header'>Notebook</div>
-    //   <div className='card-body'>
-    //     <h5 className='card-title'>R$ 2500</h5>
-    //     <p className='card-text'>
-    //       Esse é um notebook com 1tb, 16gb de ram e placa de vídeo de 16gb
-    //     </p>
-    //   </div>
-    // </div>
+  const [ativar, setAtivar] = useState(false)
 
-    //React-Bootstrap
-    <Card bg='dark' text='white' className='m-5' style={{ maxWidth: "18rem" }}>
-      <Card.Header>Notebook</Card.Header>
-      <Card.Body>
-        <Card.Title>R$ 2500</Card.Title>
-        <Card.Text>
-          Esse é um notebook com 1tb, 16gb de ram e placa de vídeo de 16gb
-        </Card.Text>
-      </Card.Body>
-    </Card>
+  return (
+    <div>
+      <button onClick={() => setAtivar(!ativar)}>Ativar</button>
+      {ativar && <Produto />}
+    </div>
   );
 };
 
--- app/src/index.js
+++ app/src/index.js
@@ -2,9 +2,6 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
