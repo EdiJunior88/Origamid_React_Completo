03eaaafd4ab51b6ac560173748f7dee2ddf1b5ad
EdiJunior88 <er.moura88@yahoo.com.br>
2023-02-22 09:58:00 -0300

    0501 CSS Import

--- app/src/App.js
+++ app/src/App.js
@@ -1,88 +1,12 @@
-import React from 'react';
-import Radio from './Form/Radio';
-
-const perguntas = [
-  {
-    pergunta: 'Qual método é utilizado para criar componentes?',
-    options: [
-      'React.makeComponent()',
-      'React.createComponent()',
-      'React.createElement()',
-    ],
-    resposta: 'React.createElement()',
-    id: 'p1',
-  },
-  {
-    pergunta: 'Como importamos um componente externo?',
-    options: [
-      'import Component from "./Component"',
-      'require("./Component")',
-      'import "./Component"',
-    ],
-    resposta: 'import Component from "./Component"',
-    id: 'p2',
-  },
-  {
-    pergunta: 'Qual hook não é nativo?',
-    options: ['useEffect()', 'useFetch()', 'useCallback()'],
-    resposta: 'useFetch()',
-    id: 'p3',
-  },
-  {
-    pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
-    options: ['set', 'get', 'use'],
-    resposta: 'use',
-    id: 'p4',
-  },
-];
+import React from "react";
+import Title from "./Title";
+import "./App.css";
 
 const App = () => {
-  const [respostas, setRespostas] = React.useState({
-    p1: '',
-    p2: '',
-    p3: '',
-    p4: '',
-  });
-  const [slide, setSlide] = React.useState(0);
-  const [resultado, setResultado] = React.useState(null);
-
-  function handleChange({ target }) {
-    setRespostas({ ...respostas, [target.id]: target.value });
-  }
-
-  function resultadoFinal() {
-    const corretas = perguntas.filter(
-      ({ id, resposta }) => respostas[id] === resposta,
-    );
-    setResultado(`Você acertou: ${corretas.length} de ${perguntas.length}`);
-  }
-
-  function handleClick() {
-    if (slide < perguntas.length - 1) {
-      setSlide(slide + 1);
-    } else {
-      setSlide(slide + 1);
-      resultadoFinal();
-    }
-  }
-
   return (
-    <form onSubmit={(event) => event.preventDefault()}>
-      {perguntas.map((pergunta, index) => (
-        <Radio
-          active={slide === index}
-          key={pergunta.id}
-          value={respostas[pergunta.id]}
-          onChange={handleChange}
-          {...pergunta}
-        />
-      ))}
-      {resultado ? (
-        <p>{resultado}</p>
-      ) : (
-        <button onClick={handleClick}>Próxima</button>
-      )}
-    </form>
+    <div className='container'>
+      <Title />
+    </div>
   );
 };
 
---
+++ app/src/Title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Title.css"
+
+const Title = () => {
+  return (
+    <h1 className='title'>Título</h1>
+  )
+}
+
+export default Title
