16d681ad2569b7c71d0bbcc1ec7de8246af29600
EdiJunior88 <er.moura88@yahoo.com.br>
2023-02-22 20:22:56 -0300

    0504 CSS Frameworks

--- app/src/App.js
+++ app/src/App.js
@@ -1,68 +1,29 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-
-const ProdutosContainer = styled.div`
-  display: flex;
-`;
-
-const Produto = styled.div`
-  flex: 1;
-`;
-
-const Titulo = styled.h1`
-  font-size: 2rem;
-  color: tomato;
-`;
-
-const Paragrafo = styled.p`
-  font-size: 1.5rem;
-  color: green;
-`;
-
-const Preco = styled.p`
-  background: hsl(${Math.random() * 360}, 100%, 50%);
-  color: white;
-`;
-
-const Comprar = styled.button`
-  background: ${({ ativo }) => (ativo ? "#000" : "#fff")};
-  font-size: 1rem;
-  border: 2px solid #000;
-  border-radius: 5px;
-  padding: 0.5rem;
-  color: ${({ ativo }) => (ativo ? "#fff" : "#000")};
-  cursor: pointer;
-  &:hover {
-    background: tomato;
-  }
-`;
+import React from "react";
+import Card from "react-bootstrap/Card";
 
 const App = () => {
-  const [ativo, setAtivo] = useState(false);
-
-  function handleClick() {
-    setAtivo(!ativo);
-  }
-
   return (
-    <div>
-      <Comprar ativo={ativo} onClick={handleClick}>
-        Compre Aqui
-      </Comprar>
-      <ProdutosContainer>
-        <Produto>
-          <Titulo>Notebook</Titulo>
-          <Paragrafo>Meu texto é esse</Paragrafo>
-          <Preco cor='#84e'>R$ 2000</Preco>
-        </Produto>
+    //Bootstrap padrão
+    // <div className='card bg-dark text-white m-5' style={{ maxWidth: "18rem" }}>
+    //   <div className='card-header'>Notebook</div>
+    //   <div className='card-body'>
+    //     <h5 className='card-title'>R$ 2500</h5>
+    //     <p className='card-text'>
+    //       Esse é um notebook com 1tb, 16gb de ram e placa de vídeo de 16gb
+    //     </p>
+    //   </div>
+    // </div>
 
-        <Produto>
-          <Titulo>Smartphone</Titulo>
-          <Paragrafo>Meu texto é esse</Paragrafo>
-          <Preco cor='#53d956'>R$ 1000</Preco>
-        </Produto>
-      </ProdutosContainer>
-    </div>
+    //React-Bootstrap
+    <Card bg='dark' text='white' className='m-5' style={{ maxWidth: "18rem" }}>
+      <Card.Header>Notebook</Card.Header>
+      <Card.Body>
+        <Card.Title>R$ 2500</Card.Title>
+        <Card.Text>
+          Esse é um notebook com 1tb, 16gb de ram e placa de vídeo de 16gb
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,9 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
