fa2c902cf7ff5327c37ee5df32f5fdbd1ae4e481
EdiJunior88 <er.moura88@yahoo.com.br>
2023-02-22 21:13:30 -0300

    0505 Animações 2

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
-import Produto from "./Produto";
-import "./App.css"
+import React from "react";
+import Slide from "./Slide";
 
 const App = () => {
-  const [ativar, setAtivar] = useState(false)
+  const slides = [
+    {
+      id: "slide1",
+      text: "Slide 1",
+    },
+    {
+      id: "slide2",
+      text: "Slide 2",
+    },
+    {
+      id: "slide3",
+      text: "Slide 3",
+    },
+  ];
 
   return (
     <div>
-      <button onClick={() => setAtivar(!ativar)}>Ativar</button>
-      {ativar && <Produto />}
+      <Slide slides={slides} />
     </div>
   );
 };
