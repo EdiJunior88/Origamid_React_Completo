a782660ee26fb1dfee933426229092f0025a5d76
EdiJunior88 <er.moura88@yahoo.com.br>
2023-02-28 17:39:11 -0300

    0506 Imagens

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,24 @@
-import React from "react";
-import Slide from "./Slide";
+import React, { useState } from "react";
+import "./App.css";
+import foto from "./img/foto.jpg";
+import DogSVG from "./DogSVG";
 
 const App = () => {
-  const slides = [
-    {
-      id: "slide1",
-      text: "Slide 1",
-    },
-    {
-      id: "slide2",
-      text: "Slide 2",
-    },
-    {
-      id: "slide3",
-      text: "Slide 3",
-    },
-  ];
+  const [olho, setOlho] = useState(0);
+
+  function handleClick() {
+    for (let i = 0; i < 6; i++) {
+      setTimeout(() => {
+        setOlho(i);
+      }, 30 * i);
+    }
+  }
 
   return (
     <div>
-      <Slide slides={slides} />
+      <p className='fundo' onClick={handleClick}></p>
+      <DogSVG color='#84e' olho={olho} />
+      <img src={foto} alt='Cachorro' />
     </div>
   );
 };
