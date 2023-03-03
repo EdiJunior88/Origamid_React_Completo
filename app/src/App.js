31a1e5b88353a8044ebf91ee4de83f6896349674
EdiJunior88 <er.moura88@yahoo.com.br>
2023-03-02 21:05:01 -0300

    0604 Nested Routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProdutoAvaliacao from "./ProdutoAvaliacao";
+import ProdutoCustomizado from "./ProdutoCustomizado";
+import ProdutoDescricao from "./ProdutoDescricao";
+
 import Header from "./Header";
 import Home from "./Home";
 import Login from "./Login";
@@ -15,7 +19,11 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='sobre' element={<Sobre />} />
         <Route path='login' element={<Login />} />
-        <Route path='produto/:id' element={<Produto />} />
+        <Route path='produto/:id' element={<Produto />}>
+          <Route path='' element={<ProdutoDescricao />} />
+          <Route path='avaliacao' element={<ProdutoAvaliacao />} />
+          <Route path='customizado' element={<ProdutoCustomizado />} />
+        </Route>
         <Route path='*' element={<NaoEncontrado />} />
       </Routes>
     </BrowserRouter>
--- app/src/Produto.js
+++ app/src/Produto.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { NavLink, Outlet, useParams } from "react-router-dom";
 
 const Produto = () => {
   const params = useParams();
-  const location = useLocation();
-  const search = new URLSearchParams(location.search)
 
   return (
     <div>
       <h1>Produto: {params.id}</h1>
+      <nav>
+        <NavLink to=''>Descrição</NavLink>
+        <NavLink to='avaliacao'>Avaliação</NavLink>
+        <NavLink to='customizado'>Customizado</NavLink>
+      </nav>
+      <Outlet />
     </div>
   );
 };
