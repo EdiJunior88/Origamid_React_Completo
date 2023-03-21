a24d6a1c642583a1252de832f25c8a8b08a1e0b2
EdiJunior88 <er.moura88@yahoo.com.br>
2023-03-20 21:59:57 -0300

    0704 useReducer

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,35 @@
-import React, { useState } from "react";
-import Header from "./Header";
+import React, { useReducer } from "react";
+import Exemplo from "./Exemplo";
+
+function reducer(state, action) {
+  switch (action) {
+    case "aumentar":
+      return state + 1;
+    case "diminuir":
+      return state - 1;
+    default:
+      throw new Error("Erro action não existe");
+  }
+
+  // if (action === "aumentar") {
+  //   return state + 1;
+  // }
+
+  // if (action === "diminuir") {
+  //   return state - 1;
+  // }
+  // return new Error("Erro action não existe");
+}
 
 const App = () => {
-  const [contar, setContar] = useState(0);
+  const [state, dispatch] = useReducer(reducer, 0);
 
   return (
     <div>
-      <Header />
-      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+      <button onClick={() => dispatch("aumentar")}>+</button>
+      <button onClick={() => dispatch("diminuir")}>-</button>
+      <p>{state}</p>
+      <Exemplo />
     </div>
   );
 };
