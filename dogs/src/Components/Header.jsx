acaac5875f7564d5f86c71403d1e135ed3efde19
EdiJunior88 <er.moura88@yahoo.com.br>
2023-07-19 18:03:53 -0300

    0801 Dogs 0802 Configuração 0803 API 1 0803 API 2 0804 Estrutura Inicial 0805 Header 0806 Login Routes 0807 Form Component 0808 useForm 0809 API Fetch

--- dogs/src/Components/Header.jsx
+++ dogs/src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
-import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+import Dogs from "./Dogs";
 
 const Header = () => {
   return (
