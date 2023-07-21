bbe111163edd5649f2a071350f5ed28fdd94683d
EdiJunior88 <er.moura88@yahoo.com.br>
2023-07-21 20:25:02 -0300

    0801 Dogs 0802 Configuração 0803 API 1 0803 API 2 0804 Estrutura Inicial 0805 Header 0806 Login Routes 0807 Form Component 0808 useForm 0809 API Fetch 0810 userContext 0811 Validate Token 0812 Login Form

---
+++ dogs/src/Components/Helper/Error.jsx
@@ -0,0 +1,7 @@
+const Error = ({ error }) => {
+  if (!error) return null;
+
+  return <p style={{ color: "#f31", margin: "1rem 0" }}>{error}</p>;
+};
+
+export default Error;
--- dogs/src/Components/Login/LoginForm.jsx
+++ dogs/src/Components/Login/LoginForm.jsx
@@ -4,6 +4,9 @@ import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
 import { useContext } from "react";
 import { UserContext } from "../../UserContext.jsx";
+import Error from "../Helper/Error";
+import styles from "./LoginForm.module.css";
+import stylesBtn from "../Forms/Button.module.css";
 
 const LoginForm = () => {
   const username = useForm();
@@ -20,9 +23,9 @@ const LoginForm = () => {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form action='' onSubmit={handleSubmit}>
+    <section className='animeLeft'>
+      <h1 className='title'>Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label='Usuário' type='text' name='username' {...username} />
         <Input label='Senha' type='password' name='password' {...password} />
         {loading ? (
@@ -30,9 +33,20 @@ const LoginForm = () => {
         ) : (
           <Button>Entrar</Button>
         )}
-        {error && <p>{error}</p>}
+        <Error error={error} />
       </form>
-      <Link to='/login/criar'>Cadastro</Link>
+
+      <Link className='styles.perdeu' to='/login/perdeu'>
+        Perdeu a senha?
+      </Link>
+
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={stylesBtn.button} to='/login/criar'>
+          Cadastro
+        </Link>
+      </div>
     </section>
   );
 };
