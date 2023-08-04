a552f46bf78a2656ee8d5fac3ed217b00ecc2001
EdiJunior88 <er.moura88@yahoo.com.br>
2023-08-03 21:21:29 -0300

    0801 Dogs 0802 Configuração 0803 API 1 0803 API 2 0804 Estrutura Inicial 0805 Header 0806 Login Routes 0807 Form Component 0808 useForm 0809 API Fetch 0810 userContext 0811 Validate Token 0812 Login Form 0813 Login Background 0814 Login Criar 0815 useFetch 0816 Protected Route 0817 User Header 0818 Mobile Menu

--- dogs/src/Components/User/UserHeaderNav.jsx
+++ dogs/src/Components/User/UserHeaderNav.jsx
@@ -1,42 +1,63 @@
-import { useContext, useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { UserContext } from '../../UserContext';
+import { useContext, useEffect, useState } from "react";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 
-import styles from './UserHeaderNav.module.css';
-import IconFeed from '../SVG/IconFeed';
-import IconStatics from '../SVG/IconStatics';
-import IconAdd from '../SVG/IconAdd';
-import IconExit from '../SVG/IconExit';
+import styles from "./UserHeaderNav.module.css";
+import IconFeed from "../SVG/IconFeed";
+import IconStatics from "../SVG/IconStatics";
+import IconAdd from "../SVG/IconAdd";
+import IconExit from "../SVG/IconExit";
+import useMedia from "../../Hooks/useMedia";
 
 const UserHeaderNav = () => {
   const { userLogout } = useContext(UserContext);
+  const mobile = useMedia("(max-width: 40rem)");
+  const [mobileMenu, setMobileMenu] = useState(false);
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   function handleLogout() {
     userLogout();
-    navigate('/login');
+    navigate("/login");
   }
-  const [mobile, setMobile] = useState(null);
+
+  useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
 
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/conta" end>
-        <IconFeed />
-        {mobile && 'Minhas Fotos'}
-      </NavLink>
-      <NavLink to="/conta/estatisticas">
-        <IconStatics />
-        {mobile && 'Estatísticas'}
-      </NavLink>
-      <NavLink to="/conta/postar">
-        <IconAdd />
-        {mobile && 'Adicionar Foto'}
-      </NavLink>
-      <button onClick={handleLogout}>
-        <IconExit />
-        {mobile && 'Sair'}
-      </button>
-    </nav>
+    <>
+      {mobile && (
+        <button
+          aria-label='Menu'
+          className={`${styles.mobileButton} ${
+            mobileMenu && styles.mobileButtonActive
+          }`}
+          onClick={() => setMobileMenu(!mobileMenu)}></button>
+      )}
+
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${
+          mobileMenu && styles.navMobileActive
+        }`}>
+        <NavLink to='/conta' end>
+          <IconFeed />
+          {mobile && "Minhas Fotos"}
+        </NavLink>
+        <NavLink to='/conta/estatisticas'>
+          <IconStatics />
+          {mobile && "Estatísticas"}
+        </NavLink>
+        <NavLink to='/conta/postar'>
+          <IconAdd />
+          {mobile && "Adicionar Foto"}
+        </NavLink>
+        <button onClick={handleLogout}>
+          <IconExit />
+          {mobile && "Sair"}
+        </button>
+      </nav>
+    </>
   );
 };
 
---
+++ dogs/src/Hooks/useMedia.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+const useMedia = (media) => {
+  const [match, setMatch] = useState(null);
+
+  useEffect(() => {
+    function changeMatch() {
+      const { matches } = window.matchMedia(media);
+      setMatch(matches);
+    }
+
+    changeMatch();
+
+    window.addEventListener("resize", changeMatch);
+
+    return () => {
+      window.removeEventListener("resize", changeMatch);
+    };
+  }, [media]);
+
+  return match;
+};
+
+export default useMedia;
