89cfd21b2d79f3c6f0fbadc062dcce9fa86d1d39
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-25 21:05:57 -0300

    Update

--- dogs/src/Components/Photo/Photo.jsx
+++ dogs/src/Components/Photo/Photo.jsx
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import useFetch from "../../Hooks/useFetch";
-import Error from "../Helper/Error";
-import Loading from "../Helper/Loading";
-import PhotoContent from "./PhotoContent";
-import Head from "../Helper/Head";
-import { PHOTO_GET } from "../../Api";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import useFetch from '../../Hooks/useFetch';
+import { PHOTO_GET } from '../../Api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import PhotoContent from './PhotoContent';
+import Head from '../Helper/Head';
 
 const Photo = () => {
   const { id } = useParams();
   const { data, loading, error, request } = useFetch();
 
-  useEffect(() => {
+  React.useEffect(() => {
     const { url, options } = PHOTO_GET(id);
     request(url, options);
   }, [request, id]);
@@ -20,7 +20,7 @@ const Photo = () => {
   if (loading) return <Loading />;
   if (data)
     return (
-      <section className='container mainContainer'>
+      <section className="container mainContainer">
         <Head title={data.photo.title} />
         <PhotoContent single={true} data={data} />
       </section>
